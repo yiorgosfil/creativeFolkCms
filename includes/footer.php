61d36c7656c6398d95d89d2a38ae1121d6ba1a58
Yiorgos <yiorgosdev@gmail.com>
2024-07-29 19:24:50 +0200

    add: header && footer

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+        <footer>
+            <div class="container">
+                &copy; Creative Folk <?= date('Y') ?>
+            </div>
+            <script src="js/site.js"></script>
+        </footer>
+        </body>
+
+        </html>
